54fef176c35a8e36eb48df18afe7c8211c4e5ad9
AndresFE0209 <afernandeze10@iesalbarregas.es>
2026-04-16 21:06:40 +0200

    creacion tarjeta personal

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,47 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [MatButtonToggleModule, MatCardModule],
+  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-  selloPuesto = false;
-
-  datos= {
+  datos = {
     pais: 'ESPAÑA',
-    nombre: 'Andres Fernandez Exposito',
+    nombre: 'Andres Fernandez',
     rol: 'FullStack Dev',
     id: 'W2M-ANG-0825',
     nac: '1991-09-02',
     exp: '2026-03-23'
   };
-
-  moverSello(event: MouseEvent, selloHerramienta: Element): void {
-    const tarjeta = event.currentTarget as Element;
-    const rect = tarjeta.getBoundingClientRect();
-
-    const x = event.clientX - rect.left - 30;
-    const y = event.clientY - rect.top - 55;
-
-    gsap.to(selloHerramienta, {
-      x,
-      y,
-      duration: 0.12,
-      ease: 'power1.out'
-    });
-  }
-
-  ponerSello(selloFinal: Element): void {
-    this.selloPuesto = false;
-
-    gsap.set(selloFinal, {
-      opacity: 0,
-      scale: 1.3,
-      rotate: -12
-    })
-  }
 }
